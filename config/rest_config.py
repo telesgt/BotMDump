dbaf31f3f854ba303bf026a200924e1c318a7e9a
Zeniker <gui.lls@hotmail.com>
2024-10-24 21:49:38 -0300

    Adicionado rate limit para apis do jikan

--- config/rest_config.py
+++ config/rest_config.py
@@ -1,5 +1,11 @@
+from requests_ratelimiter import LimiterSession
+
 class RestConfig:
 	BASE_URL_JIKAN="https://api.jikan.moe/v4/"
 
 	def __setattr__(self, name, value):
 		raise TypeError("Constants are immutable")
+	
+	@staticmethod
+	def getJikanLimiter() -> LimiterSession:
+		return LimiterSession(per_second=3, per_minute=60)
