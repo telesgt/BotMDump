7aa4e30e3aef662336c3d2d04be8a35cdc88b403
Zeniker <gui.lls@hotmail.com>
2024-10-25 22:17:52 -0300

    Substituída integração do jikan pelo anilist

--- character_dump/dump.py
+++ character_dump/dump.py
@@ -1,14 +1,6 @@
-import os
-import logging
-import json
-from json import JSONEncoder
-
-from infrastructure.jikan.character_api import CharacterApi as JikanApi
-from infrastructure.acdb.character_api import CharacterApi as AcdbApi
 from infrastructure.mongo.character_repository import CharacterRepository
+from infrastructure.anilist.character_api import CharacterApi as AnilistApi
 
-from model.jikan.character_search_request import CharacterSearchRequest as JikanRequest
-from model.acdb.character_search_request import CharacterSearchRequest as AcdbRequest
 from model.mongo.character import Character
 from model.mongo.character_images import CharacterImages
 from model.mongo.image import Image
@@ -16,9 +8,8 @@ from model.mongo.image import Image
 class CharacterDump():
 
 	def __init__(self) -> None:
-		self.character_repository = CharacterRepository()
-		self.jikan_api = JikanApi()
-		self.acdb_api = AcdbApi()
+		self.character_repository = CharacterRepository()		
+		self.anilist_api = AnilistApi()
 	
 	def doDump(self, total_personagens):
 
@@ -34,61 +25,42 @@ class CharacterDump():
 
 			page += 1
 
-			jikan_request = self.get_request_jikan(page)
-			jikan_response = self.jikan_api.search(jikan_request)
+			anilist_response = self.anilist_api.search(page)
 
-			for jikan_character in jikan_response['data']:
+			for anilist_character in anilist_response['data']['Page']['characters']:
 
-				contador_personagens += 1
-				
-				# acdb_request = self.get_request_acdb(jikan_character['name'])
-				# acdb_character = self.acdb_api.find_by_name(acdb_request)
+				contador_personagens += 1				
 
-				mongo_character = self.create_mongo_character(jikan_character, raridade=self.get_raridade_personagem(raridades, contador_personagens))
+				mongo_character = self.create_mongo_character(anilist_character, raridade=self.get_raridade_personagem(raridades, contador_personagens))
 				self.character_repository.insert_waifu(mongo_character)
+
+				if contador_personagens >= total_personagens:
+					break
 			
-			if jikan_response['pagination']['has_next_page'] == False:
+			if anilist_response['data']['Page']['pageInfo']['hasNextPage'] == False:
 				print('Sem mais personagens para buscar')
 				break
 
 			
 
-	def create_mongo_character(self, jikan_character, acdb_character=None, raridade=None) -> Character:
+	def create_mongo_character(self, anilist_character, raridade=None) -> Character:
 
 		image = Image()
-		image.image_url = jikan_character['images']['jpg']['image_url']
-		image.small_image_url = jikan_character['images']['jpg'].get('small_image_url')
+		image.image_url = anilist_character['image']['large']
+		image.small_image_url = anilist_character['image']['medium']
 
 		character_images = CharacterImages()
 		character_images.jpg = image
 		
 		mongo_character = Character()
 		mongo_character.images = character_images
-		mongo_character.name = jikan_character['name']
-		mongo_character.name_kanji = jikan_character['name_kanji']
-		mongo_character.nicknames = jikan_character['nicknames']
-		mongo_character.rarity = raridade
+		mongo_character.name = anilist_character['name']['full']
+		mongo_character.name_kanji = anilist_character['name']['native']
+		mongo_character.nicknames = anilist_character['name']['alternative']
+		mongo_character.gender = anilist_character['gender']
+		mongo_character.rarity = raridade		
 
-		if acdb_character != None:
-			mongo_character.gender = acdb_character['gender']
-
-		return mongo_character
-	
-	def get_request_jikan(self, page) -> JikanRequest:
-
-		character_request = JikanRequest()		
-		character_request.page = page
-		character_request.order_by = "favorites"
-		character_request.sort = "desc"
-
-		return character_request
-	
-	def get_request_acdb(self, character_name : str) -> AcdbRequest:
-
-		character_request = AcdbRequest()		
-		character_request.character_q = character_name
-
-		return character_request
+		return mongo_character	
 	
 	def calcular_valores_raridade(self, total_personagens : int):
 
--- config/rest_config.py
+++ config/rest_config.py
@@ -3,6 +3,7 @@ from requests_ratelimiter import LimiterSession
 class RestConfig:
 	BASE_URL_JIKAN="https://api.jikan.moe/v4/"
 	BASE_URL_ACDB="https://www.animecharactersdatabase.com/"
+	BASE_URL_ANILIST="https://graphql.anilist.co"
 	
 	@staticmethod
 	def get_jikan_limiter() -> LimiterSession:
@@ -10,6 +11,9 @@ class RestConfig:
 	
 	def get_acdb_limiter() -> LimiterSession:
 		return LimiterSession(per_second=1)
+	
+	def get_anilist_limiter() -> LimiterSession:
+		return LimiterSession(per_minute=20)
 	
 	@staticmethod
 	def get_headers():
